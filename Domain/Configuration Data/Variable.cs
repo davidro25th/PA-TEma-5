0af97b047d516c5a040b867354bfbc9849c6fd0d
Frank Leyva <frankkleyva@gmail.com>
2024-06-03 21:47:05 -0700

    clase variable

---
+++ Domain/Configuration Data/Variable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Variable
+{
+	public Variable()
+	{
+		public string name {  get; set; }
+	    public string code { get; set; }
+		public string MeasurementUnit { get; set; }
+
+	}
+}
