fc8c62e5a7f4e732a4f8820ba16c7be68a969e5f
David <David@DESKTOP-OEL89OB>
2024-06-04 01:35:51 -0400

    Adding New Classes (Worker and Variable)

--- feature/Configuration_Data/Variable.cs
+++ feature/Configuration_Data/Variable.cs
@@ -2,16 +2,17 @@
 
 public class Variable
 {
-	public Variable()
+	public Variable( string code )
 	{
-
-		protected string name;
-		protected string code;
-		protected Measure_Units measure_Units;
-
-		public string Name { get { return name; } };
-		public string Code { get { return code; } };
-		public string Measure_Units { get { return measure_Units; } };
-
+		this.code = code;
 	}
+
+	protected string name;
+	protected string code;
+	protected Measure_Units measure_Units;
+
+	public string Name { get { return name; } };
+	public string Code { get { return code; } };
+	public string Measure_Units { get { return measure_Units; } };
+
 }
---
+++ feature/Configuration_Data/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Worker
+{
+	public Worker( string id )
+	{
+		this.id = id;
+	}
+
+	protected string firstname;
+	protected string lastname;
+	protected string id;
+	protected string phone_number;
+
+	public string Firstname { get { return firstname; } }
+	public string Lastname { get { return lastname; } }
+	protected string Id { get { return Id; } }
+	public string PhoneNumber { get { return phone_number;} }
+
+}
